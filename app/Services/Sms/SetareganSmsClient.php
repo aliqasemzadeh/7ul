3ccc8139fa375442a7e431e3ea71adc15769748b
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-08-06 20:44:27 +0330

    Add SMS logging system with related migrations, models, services, jobs, and tests

---
+++ app/Services/Sms/SetareganSmsClient.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services\Sms;
+
+class SetareganSmsClient
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
