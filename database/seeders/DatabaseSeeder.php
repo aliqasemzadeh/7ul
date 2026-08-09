98feb3331d1983caa47f1290aef4a1c6eef700b6
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2026-08-09 07:24:42 +0330

    Assign `admin` role to default user in `DatabaseSeeder` for initial setup

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,8 +16,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory()->create([
-            'mobile' => '09121234567',
-        ]);
+        $adminRole = Role::firstOrCreate(['name' => 'admin']);
+
+        $user = User::firstOrCreate(
+            ['mobile' => '09121234567'],
+        );
+
+        $user->assignRole($adminRole);
     }
 }
